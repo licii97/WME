639e0d213a4786c875a67fda086752e8ea26019b
Felicitas <licii97@web.de>
2018-10-16 12:11:27 +0200

    einheitliches Wackeln

--- Aufgabe 1/script.js
+++ Aufgabe 1/script.js
@@ -1,7 +1,9 @@
 <script>
-    var id;
 
+
+/* Funktion um Tabellenspalten sichtbar und unsichtbar zu machen */
     function visibilityFct() {
+        var id;
         document.getElementById("birthRate").style.display="none";
     }
 </script>
