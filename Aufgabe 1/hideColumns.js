e3688b299a651cce8a190913e9df7092088dd3a0
Verona Kolpe <verona@kolpe.de>
2018-11-04 12:05:35 +0100

    aufgeräumter code

--- Aufgabe 1/hideColumns.js
+++ Aufgabe 1/hideColumns.js
@@ -1,54 +1,43 @@
 window.addEventListener('resize', function() {
   var width = window.innerWidth;
+
   if (width > 1200) {
     var classNames = ['children', 'electricUsage', 'internetUsage'];
-    for (var i = 0; i < classNames.length; i++ ){
-        var listOfColumns = document.getElementsByClassName(classNames[i]);
-        for (var j = 0; j < listOfColumns.length; j++){
-          listOfColumns[j].style.display="table-cell";
-        }
-    }
+
+    coloumnsDisplayTableCell(classNames);
   }
   else if (width < 1200 && width > 768 ) {
-        var classNames = ['children', 'electricUsage'];
-        coloumnsDisplayNone('internetUsage');
+    var classNames = ['children', 'electricUsage'];
 
-        for (var i = 0; i < classNames.length; i++ ){
-            var listOfColumns = document.getElementsByClassName(classNames[i]);
-            for (var j = 0; j < listOfColumns.length; j++){
-              listOfColumns[j].style.display="table-cell";
-            }
-        }
+    coloumnsDisplayNone('internetUsage');
+    coloumnsDisplayTableCell(classNames);
   }
   else if (width < 768 && width > 320 ) {
-        var classNames = ['children'];
-        coloumnsDisplayNone('internetUsage');
-        coloumnsDisplayNone('electricUsage');
+    var classNames = ['children'];
 
-        for (var i = 0; i < classNames.length; i++ ){
-            var listOfColumns = document.getElementsByClassName(classNames[i]);
-            for (var j = 0; j < listOfColumns.length; j++){
-              listOfColumns[j].style.display="table-cell";
-            }
-        }
+    coloumnsDisplayNone('internetUsage');
+    coloumnsDisplayNone('electricUsage');
+    coloumnsDisplayTableCell(classNames);
   }
   else if (width < 320) {
-        coloumnsDisplayNone('internetUsage');
-        coloumnsDisplayNone('electricUsage');
-        coloumnsDisplayNone('children');
+    coloumnsDisplayNone('internetUsage');
+    coloumnsDisplayNone('electricUsage');
+    coloumnsDisplayNone('children');
   }
 });
 
 function coloumnsDisplayNone (nameOfClass) {
   var listOfColumns = document.getElementsByClassName(nameOfClass);
+
   for (var j = 0; j < listOfColumns.length; j++){
     listOfColumns[j].style.display="none";
   }
 }
 
-function coloumnsDisplayTableCell (number) {
-  for (var i = 0; i < (classNames.length - number); i++ ){
+function coloumnsDisplayTableCell (classNames) {
+  for (var i = 0; i < classNames.length; i++ ){
       var listOfColumns = document.getElementsByClassName(classNames[i]);
+      
       for (var j = 0; j < listOfColumns.length; j++){
         listOfColumns[j].style.display="table-cell";
       }
