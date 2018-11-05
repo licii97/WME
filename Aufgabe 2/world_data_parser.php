edcded35b2276669efcdc2a3199cb45acd71b938
Felicitas Schmelz <licii97@web.de>
2018-11-05 10:58:14 +0100

    erste funktion parsecsv()

--- Aufgabe 2/world_data_parser.php
+++ Aufgabe 2/world_data_parser.php
@@ -1,9 +1,25 @@
 <?php
 class WorldDataParser  {
-	public function parseCSV()  
+	public function parseCSV($pfad)  
 	{
 		//array fgetcsv(resource $handle, integer $length, string $delimiter)
+		//return array; 
+		//$data; 
+		$row = 1;
+		if (($handle = fopen("world_data_v1.csv", "r")) !== FALSE) {
+			while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+				$num = count($data);
+				echo "<p> $num Felder in Zeile $row: <br /></p>\n";
+				$row++;
+				for ($c=0; $c < $num; $c++) {
+					echo $data[$c] . "<br />\n";
+				}
+			}
+			fclose($handle);
+		}
+		//return $data; 
 	}
+	
 	public function saveXML()
 	{
 	//XML Datei soll im gleichen Ordner als  world_data.xml gespeichert werden
