b12331445cd61fa470978ea63cf692c04ba78a89
Verona Kolpe <verona@kolpe.de>
2018-11-27 14:17:35 +0100

    neue packages und kram

--- Aufgabe 3/Team_00/app.js
+++ Aufgabe 3/Team_00/app.js
@@ -17,26 +17,20 @@ app.use( express.static( path.join(__dirname, "public") ) );
 
 
 //https://www.npmjs.com/package/csvtojson
-const csvToJsonObj;
+var csvToJsonObj = {};
 const csvFilePath='./world_data.csv';
 const csv=require('csvtojson');
-csv()
-.fromFile(csvFilePath)
-.then((jsonObj)=>{
-    console.log(jsonObj);
-    csvToJsonObj = jsonObj;
-
-})
-
-// Async / await usage
-const jsonArray = await csv().fromFile(csvFilePath);
+csv().fromFile(csvFilePath)
+			.then(function(jsonObj){
+				csvToJsonObj = jsonObj;
+			})
 
 /**************************************************************************
 ********************** handle HTTP METHODS ***********************
 **************************************************************************/
 
-$id = $_GET["id"];
-$range = $_GET["range"];
+/*$id = $_GET["id"];
+$range = $_GET["range"];*/
 
 //jetzt muss man wahrscheinlich irgendwo im json objekt den scheiß suchen mit id und range & das zurückgeben
 // also echo: "<table>";
@@ -51,3 +45,42 @@ var server = app.listen(3000, function () {
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
 });
+
+app.get('/items', function(req, res) {
+		res.contentType('application/json'); //default ist text/html, deswegen extra angeben
+		res.send(csvToJsonObj); //gibt das jsonObj zurück, also die daten, die könnte man jetzt vorher noch bearbeiten, also irgendwas löschen oder so
+})
+
+app.get('/items/:id', function (req, res) { // wenn man var haben möchte, dann muss noch ein : davor
+	res.contentType('application/json');	
+	var id = req.params.id;
+	//aus dem jsob das passende ding raussuchen
+})
+
+app.get('/items/:id1/:id2', function (req, res) {
+	res.contentType('application/json');	
+	var id_1 = req.params.id1;
+	var id_2 = req.params.id2;
+})
+
+app.get('/properties', function (req, res) {
+	res.contentType('application/json');	
+})
+
+app.get('/properties/:num', function (req, res) {
+	res.contentType('application/json');	
+	var num = req.params.num;
+})
+
+app.post('/items', function (req, res) {
+	res.send('Added country {name} to list!');
+})
+
+app.delete('/items', function (req, res) {
+	res.send('Deleted last country: {name}!');
+})
+
+app.delete('/items/:id', function (res, req) {
+	res.send('Item {id} deleted successfully.');
+	res.send('No such id {id} in database');
+})
