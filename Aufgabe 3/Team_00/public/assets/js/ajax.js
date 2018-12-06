635155257e38a2d72eb91291f48f2ad02b723a9d
Verona Kolpe <verona@kolpe.de>
2018-12-06 11:50:28 +0100

    show/hide funktioniert, außer für th-tag

--- Aufgabe 3/Team_00/public/assets/js/ajax.js
+++ Aufgabe 3/Team_00/public/assets/js/ajax.js
@@ -2,7 +2,21 @@ function pad(n) {
   n = n + '';
   return n.length >= 3 ? n : new Array(3 - n.length + 1).join(0) + n;
 }
+function visibilityFct(id) {
+    var wert;
+    var liste = document.getElementsByClassName(id);
 
+    if (liste[0].style.display!="none"){
+        wert="none";
+    }
+    else{
+        wert="table-cell";
+    }
+
+    for (var index=0 ; index<liste.length; index++){
+        liste[index].style.display=wert;
+    }
+}
 $('document').ready(function(e){
   $.ajax({
 			type: "GET",
@@ -27,13 +41,22 @@ $('document').ready(function(e){
   				$("#table_body").html("");
   				$.each(data, function(index, element){
   					$("#table_body").append("<tr id=" + index + "></tr>");
-  					$("#" + index).append("<td>" + element["id"] + "</td>");
-  					$("#" + index).append("<td>" + element["name"] + "</td>");
-  					$("#" + index).append("<td>" + element["birth_rate_per_1000"] + "</td>");
-  					$("#" + index).append("<td>" + element["cell_phones_per_100"] + "</td>");
-  					$("#" + index).append("<td>" + element["children_per_woman"] + "</td>");
-  					$("#" + index).append("<td>" + element["electricity_consumption_per_capita"] + "</td>");
-  					$("#" + index).append("<td>" + element["internet_user_per_100"] + "</td>");
+  					$("#" + index).append("<td class=&quot;id&quot;>" + element["id"] + "</td>");
+  					$("#" + index).append("<td class=&quot;name&quot;>" + element["name"] + "</td>");
+  					$("#" + index).append("<td class=&quot;birth_rate_per_1000&quot;>" + element["birth_rate_per_1000"] + "</td>");
+  					$("#" + index).append("<td class=&quot;cell_phones_per_100&quot; >" + element["cell_phones_per_100"] + "</td>");
+  					$("#" + index).append("<td class=&quot;children_per_woman&quot;>" + element["children_per_woman"] + "</td>");
+  					$("#" + index).append("<td class=&quot;electricity_consumption_per_capita&quot;>" + element["electricity_consumption_per_capita"] + "</td>");
+            $("#" + index).append("<td class=&quot;gdp_per_capita&quot;>" + element["gdp_per_capita"] + "</td>");
+            $("#" + index).append("<td class=&quot;gdp_per_capita_growth&quot;>" + element["gdp_per_capita_growth"] + "</td>");
+            $("#" + index).append("<td class=&quot;inflation_annual&quot;>" + element["inflation_annual"] + "</td>");
+            $("#" + index).append("<td class=&quot;internet_user_per_100&quot;>" + element["internet_user_per_100"] + "</td>");
+            $("#" + index).append("<td class=&quot;life_expectancy&quot;>" + element["life_expectancy"] + "</td>");
+            $("#" + index).append("<td class=&quot;military_expenditure_percent_of_gdp&quot;>" + element["military_expenditure_percent_of_gdp"] + "</td>");
+            $("#" + index).append("<td class=&quot;gps_lat&quot;>" + element["gps_lat"] + "</td>");
+  					$("#" + index).append("<td class=&quot;gps_long&quot;>" + element["gps_long"] + "</td>");
+
+
   				});
   			}, error: function(jqXHR, text, err) {
   			}
@@ -67,14 +90,21 @@ $("#add_submit").click(function(e){
 			success: function(data) {
 				$("#table_body").html("");
 				$.each(data, function(index, element){
-					$("#table_body").append("<tr id=" + index + "></tr>");
-					$("#" + index).append("<td>" + element["id"] + "</td>");
-					$("#" + index).append("<td>" + element["name"] + "</td>");
-					$("#" + index).append("<td>" + element["birth_rate_per_1000"] + "</td>");
-					$("#" + index).append("<td>" + element["cell_phones_per_100"] + "</td>");
-					$("#" + index).append("<td>" + element["children_per_woman"] + "</td>");
-					$("#" + index).append("<td>" + element["electricity_consumption_per_capita"] + "</td>");
-					$("#" + index).append("<td>" + element["internet_user_per_100"] + "</td>");
+          $("#table_body").append("<tr id=" + index + "></tr>");
+          $("#" + index).append("<td class=&quot;id&quot;>" + element["id"] + "</td>");
+          $("#" + index).append("<td class=&quot;name&quot;>" + element["name"] + "</td>");
+          $("#" + index).append("<td class=&quot;birth_rate_per_1000&quot;>" + element["birth_rate_per_1000"] + "</td>");
+          $("#" + index).append("<td class=&quot;cell_phones_per_100&quot; >" + element["cell_phones_per_100"] + "</td>");
+          $("#" + index).append("<td class=&quot;children_per_woman&quot;>" + element["children_per_woman"] + "</td>");
+          $("#" + index).append("<td class=&quot;electricity_consumption_per_capita&quot;>" + element["electricity_consumption_per_capita"] + "</td>");
+          $("#" + index).append("<td class=&quot;gdp_per_capita&quot;>" + element["gdp_per_capita"] + "</td>");
+          $("#" + index).append("<td class=&quot;gdp_per_capita_growth&quot;>" + element["gdp_per_capita_growth"] + "</td>");
+          $("#" + index).append("<td class=&quot;inflation_annual&quot;>" + element["inflation_annual"] + "</td>");
+          $("#" + index).append("<td class=&quot;internet_user_per_100&quot;>" + element["internet_user_per_100"] + "</td>");
+          $("#" + index).append("<td class=&quot;life_expectancy&quot;>" + element["life_expectancy"] + "</td>");
+          $("#" + index).append("<td class=&quot;military_expenditure_percent_of_gdp&quot;>" + element["military_expenditure_percent_of_gdp"] + "</td>");
+          $("#" + index).append("<td class=&quot;gps_lat&quot;>" + element["gps_lat"] + "</td>");
+          $("#" + index).append("<td class=&quot;gps_long&quot;>" + element["gps_long"] + "</td>");
 				});
 			}, error: function(jqXHR, text, err) {
 			}
@@ -82,11 +112,23 @@ $("#add_submit").click(function(e){
 });
 
 
-$("show_selected_prop").click(function(){
-		//soll das machen, was getan werden soll, wenn "show" geklickt wird
+$("#show_selected_prop").click(function(e){
+  e.preventDefault();
+  var propSelection = document.getElementById("prop_selection");
+  var id = propSelection.options[propSelection.selectedIndex].value;
+
+  console.log(id);
+
+  visibilityFct(id);
 	});
-$("hide_selected_prop").click(function(){
-		//soll das machen, was getan werden soll, wenn auf "hide" geklickt wird
+$("#hide_selected_prop").click(function(e){
+  e.preventDefault();
+  var propSelection = document.getElementById("prop_selection");
+  var id = propSelection.options[propSelection.selectedIndex].value;
+
+  console.log(id);
+
+  visibilityFct(id);
 	});
 $("add_submit").click(function(){
 	var country_name = $("#country_name").val();
